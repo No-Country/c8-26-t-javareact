7ab516ecf64acb4ce45ac793a733f48cf0ceb610
Rementeria03 <waltrementeriaq@gmail.com>
2022-11-17 16:20:24 -0300

    first commit front

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Container, Input, InputGroup, Stack } from "@chakra-ui/react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <></>
+  );
+}
+
+export default App;
