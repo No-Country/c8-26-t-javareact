eb8745c1d243ec4fa95922cbf57d93e9c3c9547e
Andres Orozco <juanxxi2015@gmail.com>
2022-11-17 19:20:56 -0500

    Service changed

--- back/src/main/java/com/nocountry/entidades/Service.java
+++ back/src/main/java/com/nocountry/entidades/Service.java
@@ -2,4 +2,16 @@ package com.nocountry.entidades;
 
 public class Service {
 
+    @NotNull(message = "No puede ser nulo")
+	@NotEmpty(message = "No puede ser vacío")
+	@Column(name = "nombre_usuario", nullable = false, length = 60)
+	private String typeOfService;
+
+    private boolean serviceIsActive;
+	
+    private Service(){
+        String s;
+        
+    }
+    //TODO Methods to be made
 }
